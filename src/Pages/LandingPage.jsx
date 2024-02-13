59e8738868e6b3c5ae6e46d2f0e98b564fafabfa
Tomas Pereira <tomaspereira2k@gmail.com>
2024-02-13 19:22:05 -0300

    added new icons + sabiaas que... section

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -26,15 +26,15 @@ const LandingPage = () => {
                 <section className="relative flex justify-center items-center overflow-hidden">
                     <img className="object-contain object-center" src="/hero.png" alt=" " />
                     <div className="absolute top-1/2">
-                        <h1 className="text-[2.625rem] max-w-[20ch]">Hace que tus días, <span className="text-darkGreen1 font-bold">tengan más sabor...</span></h1>
+                        <h1 className="text-[2.625rem] max-w-[20ch]">Hace que tus días, <span className="text-darkGreen1 font-extrabold">tengan más sabor...</span></h1>
                         <div className="relative">
                             <input className="p-4 rounded-full w-full" type="text" placeholder="Veamos que tenes cerca" />
                             <button className="absolute top-0 right-0 px-6 py-4 font-bold bg-grayishGreen3 rounded-full" type="button">Buscar</button>
                         </div>
                     </div>
                 </section>
-                <section className="p-8">
-                    <h2 className="text-[2.5rem] font-bold">Categorias</h2>
+                <section className="px-12 py-8 bg-[#edebee]">
+                    <h2 className="mb-8 text-[2.5rem] text-darkGreen1 font-extrabold">Categorias</h2>
                     <div className="grid grid-flow-col auto-cols-max justify-evenly text-center">
                         <div>
                             <div className="p-4 bg-darkGreen1 w-fit rounded-full">
@@ -72,7 +72,41 @@ const LandingPage = () => {
                             </div>
                             <p className="font-semibold mt-6">VER MÁS</p>
                         </div>
+                    </div>
+                </section>
+                <section className="px-12 py-8 ">
+                    <div className="flex justify-between items-center mb-8">
+                        <h2 className="text-[2.5rem] text-darkGreen1 font-extrabold">Productos más vendidos</h2>
+                        <a href="#">Ver más</a>
+                    </div>
+                </section>
+                <section className="flex flex-col gap-8 px-12 py-8">
+                    <div className="flex">
+                        <div className="flex flex-col">
+                            <h2 className="text-[2.5rem] font-bold max-w-[30ch]">¿Sabías que ahora podes hacer tus pedidos desde la web?</h2>
+                            <h3 className="text-lg">Comenzaron tus compras virtuales por NutriMarket</h3>
+                            <a className="px-8 py-4 mt-6 w-fit rounded-full bg-grayishGreen3" href="#">Comprar</a>
+                        </div>
+                        <img className="w-1/2" src="/guy-with-veggies-img.png" alt=" "/>
+                    </div>
 
+                    <div className="grid grid-cols-4 gap-2">
+                        <div className="flex items-center gap-4 p-4 bg-[#F4F6FA] rounded-xl">
+                            <img src="best-prices-icon.svg" alt=" " />
+                            <p>Los mejores precios y ofertas</p>
+                        </div>
+                        <div className="flex items-center gap-4 p-4 bg-[#F4F6FA] rounded-xl">
+                            <img src="free-delivery-icon.svg" alt=" " />
+                            <p>Delivery gratis</p>
+                        </div>
+                        <div className="flex items-center gap-4 p-4 bg-[#F4F6FA] rounded-xl">
+                            <img src="daily-offers-icon.svg" alt=" " />
+                            <p>Ofertas diarias</p>
+                        </div>
+                        <div className="flex items-center gap-4 p-4 bg-[#F4F6FA] rounded-xl">
+                            <img src="wide-variety-icon.svg" alt=" " />
+                            <p>Amplia variedad</p>
+                        </div>
                     </div>
                 </section>
             </main>
