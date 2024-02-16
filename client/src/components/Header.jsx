e837cbc8a14758fc5262eb037945554eb7c49ca6
Tomas Pereira <tomaspereira2k@gmail.com>
2024-02-16 19:13:52 -0300

    fixed cart icon bug, fixed rendering of the navbar bug

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -13,24 +13,25 @@ const Header = () => {
                     <a href="#">Contacto</a>
                 </nav>
             </div>
-        {isLoggedIn ?
-            <div className="flex items-center gap-2 group">
-                <div>
-                    <p className="text-sm text-darkGreen1 font-bold">Julio Rossi</p>
-                    <a className="text-xs text-grayishGreen2 font-bold" href="#"><p>Cerrar Sesión</p></a>
-                </div>
-                <img src="/pfp.svg" alt=" " />
-                <img className="rotate-90 group-hover:rotate-[270deg]" src="/arrow-icon.svg" alt=" " />
-            </div>
-        :
             <div className="flex gap-6 items-center">
                 <img src="/magnifier-icon.svg" alt=" " />
                 <img src="/cart-icon.svg" alt=" " />
-                <div>
-                    <a className="text-darkGreen1 font-bold" href="#">Inicia Sesión</a>
-                    <a className="px-6 py-3 ml-6 bg-grayishGreen3 rounded-full" href="#">Registrate</a>
-                </div>
-            </div>}
+                {isLoggedIn ?
+                    <div className="flex items-center gap-2 group">
+                    <div>
+                        <p className="text-sm text-darkGreen1 font-bold">Julio Rossi</p>
+                        <a className="text-xs text-grayishGreen2 font-bold" href="#"><p>Cerrar Sesión</p></a>
+                    </div>
+                    <img src="/pfp.svg" alt=" " />
+                    <img className="rotate-90 group-hover:rotate-[270deg]" src="/arrow-icon.svg" alt=" " />
+                    </div>
+                :
+                    <div>
+                        <a className="text-darkGreen1 font-bold" href="#">Inicia Sesión</a>
+                        <a className="px-6 py-3 ml-6 bg-grayishGreen3 rounded-full" href="#">Registrate</a>
+                    </div>
+                }
+            </div>
         </header>
     )
 }
