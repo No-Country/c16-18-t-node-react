7f334ce52a156b54df792de74c0b22347f8a01e2
Tomas Pereira <tomaspereira2k@gmail.com>
2024-02-22 02:31:11 -0300

    añadido modal de productos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
+import ProductModal from "./components/ProductModal"
 import AppRouter from "./router/AppRouter"
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
         <Header/>
         <AppRouter/>
         <Footer/>
+        <ProductModal />
         </BrowserRouter>
     )
 }
