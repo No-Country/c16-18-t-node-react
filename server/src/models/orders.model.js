75beaa2196bae916db7230e78e900edb9bdfb114
Juliana Elias <eliasjuliana91@gmail.com>
2024-02-26 15:02:20 -0300

    actualizo dev antes de hacer pr de feat/carrito

---
+++ server/src/models/orders.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import { businessCollection, usersCollection, ordersCollection } from "../config/collections.config.js";
+
+const ordersSchema = new mongoose.Schema({
+    numeroOrden:String,
+    negocio:{
+        type:mongoose.SchemaTypes.ObjectId,
+        ref:businessCollection
+    },
+    usuario:{
+        type:mongoose.SchemaTypes.ObjectId,
+        ref:usersCollection
+    },
+    products:[],
+    precioTotal:Number,
+    estado:{
+        type:String,
+        enum:["pendiente", "completado", "cancelado"],
+        default:"pendiente"
+    }
+});
+
+
+export const ordersModel = mongoose.model(ordersCollection,ordersSchema);
