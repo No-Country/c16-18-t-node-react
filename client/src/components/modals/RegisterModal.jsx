e57657d73db2e591d7a3002695fbc3c26c025e66
Tomas Pereira <tomaspereira2k@gmail.com>
2024-03-06 10:23:52 -0300

    cambios minimos

--- client/src/components/modals/RegisterModal.jsx
+++ client/src/components/modals/RegisterModal.jsx
@@ -8,7 +8,7 @@ const RegisterModal = ({ visible, onClose }) => {
   
   return (
     <div id="container" onClick={handleOnClose} className={`${visible ? 'flex' : 'hidden'} fixed top-0 z-50 w-full h-full p-4 bg-grayishGreen4 bg-opacity-75 lg:justify-center lg:items-center`}>
-      <div className="relative flex flex-col items-center h-full py-12 bg-white rounded-md overflow-y-scroll scroll-smooth lg:h-auto">
+      <div className="relative flex flex-col items-center h-full py-12 bg-white rounded-md overflow-y-scroll scroll-smooth ">
         <img className="absolute top-4 left-4 w-6 cursor-pointer" src="/cross-icon.svg" alt=" " onClick={() => {onClose()}} />
         <img src="/register.png" className="w-[210px] h-[120px] object-contain" />
         <h3 className="text-xl mb-0 lg:text-3xl font-semibold leading-10 text-center lg:mb-2">
