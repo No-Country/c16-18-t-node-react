b34a2a9f4c691f0439512ff757e139c7d41821b3
Leonardo Tolaba <leonardotolaba.20@gmail.com>
2024-03-05 21:57:30 -0300

    Corrigiendo persistencia - ltolaba

--- client/src/Pages/CartPage.jsx
+++ client/src/Pages/CartPage.jsx
@@ -2,25 +2,16 @@ import ProductsTable from "../components/Cart/ProductsTable";
 import SearchBar from "../components/Cart/SearchBar";
 import Summary from "../components/Cart/Summary";
 import Discount from "../components/Cart/Discount";
-import { AuthContext } from "../auth/context/AuthContext";
-import { useContext } from "react";
+
 
 
 const CartPage = () => {
-
-  const { user } = useContext(AuthContext);
-
-  const userId = user ? user.id || null : null;
-
-  localStorage.setItem('userId', userId);
-
-
 
   return (
     <section className="mb-5 w-full">
       <SearchBar />
       <div className="flex flex-col xl:flex-row content-center">
-        <ProductsTable userId={userId} />
+        <ProductsTable/>
         <Summary />
       </div>
       <Discount />
