d4a819dd2253b956ff45ca348f368877ca385249
Tomas Pereira <tomaspereira2k@gmail.com>
2024-03-06 23:51:54 -0300

    cambiado estilos del root

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -38,7 +38,7 @@ const Header = () => {
   };
 
   return (
-    <header className="fixed z-50 flex justify-between items-center w-full p-8 bg-white max-w-[1440px] mx-auto lg:static">
+    <header className="fixed z-50 flex justify-between items-center w-full p-8 bg-white lg:static">
       <div className="md:hidden">
         <button
           className=" flex-col justify-center items-center md:hidden"
